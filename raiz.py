aa0dc1729cad94a73166b47b1e3beaea3394c3a4
W1ll4nder <willanderguitar@hotmail.com>
2017-10-16 16:00:32 -0300

    raiz

---
+++ raiz.py
@@ -0,0 +1,24 @@
+m = int(input('digite um numero inteiro: '))
+num = 1
+lista = [num]
+for n in range(1, m+1):
+    cont = True
+    if num%2 == 0:
+        num+=1
+    else:
+        print(' ')
+    while cont == True:
+        if sum(lista) == n**3:
+            print('{}**3 = {}'.format(n, lista))
+            num +=2
+            lista = [num]
+            cont = False
+        elif sum(lista)<n**3:
+            num+=2
+            lista.append(num)
+        elif sum(lista)>n**3:
+            del lista[0]
+
+
+
+
