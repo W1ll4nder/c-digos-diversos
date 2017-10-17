a7be520c44fe8088325e7411ba4f129f5679adac
W1ll4nder <willanderguitar@hotmail.com>
2017-10-17 09:24:10 -0300

    correção

--- primos de 1 ate m.py
+++ primos de 1 ate m.py
@@ -3,9 +3,7 @@
 m = int(input(' digite um numero inteiro: '))
 cont = []
 for n in range(2, m+1):
-   #print(n)
    for i in range(1, n+1):
-       #print(i)
        if n%i==0:
            cont.append(1)
    if sum(cont) == 2:
